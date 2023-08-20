f4cec7eb19a9e0d2221f0fa602f286e1edd8a930
dasun <amarasinghe.dasun61@gmail.com>
2023-08-20 16:59:54 +0530

    Initializing interface

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {AppComponent} from "./app.component";
 
 const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers:[],
+  bootstrap:[AppComponent]
 })
 export class AppRoutingModule { }
